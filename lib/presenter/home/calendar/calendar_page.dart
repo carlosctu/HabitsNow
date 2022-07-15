ec065be846944b74d6fb0bba335d8fb57fb77200
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-07-15 14:17:06 -0300

    [feat] creating my branch

---
+++ lib/presenter/home/calendar/calendar_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Calendar_Page extends StatefulWidget {
+  const Calendar_Page({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar_Page> createState() => _Calendar_PageState();
+}
+
+class _Calendar_PageState extends State<Calendar_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
