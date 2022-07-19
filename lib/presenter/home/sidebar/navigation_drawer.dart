3793b2607b5b985a62d72112e53f7662aa64094a
carlosctu <carlosctu94@gmail.com>
2022-07-18 22:28:35 -0300

    [FEAT] Become Premium AlertBox

--- lib/presenter/home/sidebar/navigation_drawer.dart
+++ lib/presenter/home/sidebar/navigation_drawer.dart
@@ -113,15 +113,22 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                   buildSideBarItem(context,
                       item: NavigationItem.becomePremium,
                       text: 'Obtenha Premium',
-                      icon: Icons.verified_outlined,
-                      onClicked: () {}),
+                      icon: Icons.verified_outlined, onClicked: () {
+                    BePremium alert = const BePremium();
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return alert;
+                      },
+                    );
+                  }),
                   buildSideBarItem(context,
                       item: NavigationItem.rateUs,
                       text: 'Avalie o aplicativo',
                       icon: Icons.rate_review,
                       // ignore: avoid_print
                       onClicked: () {
-                    ReportBug alert = const ReportBug();
+                    BePremium alert = const BePremium();
                     showDialog(
                       context: context,
                       builder: (BuildContext context) {
@@ -180,8 +187,8 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
   }
 }
 
-class ReportBug extends StatelessWidget {
-  const ReportBug({
+class RateOurApp extends StatelessWidget {
+  const RateOurApp({
     Key? key,
   }) : super(key: key);
 
@@ -272,6 +279,94 @@ class ReportBug extends StatelessWidget {
                     color: Color.fromARGB(255, 213, 32, 89),
                     fontSize: 15,
                     fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class BePremium extends StatelessWidget {
+  const BePremium({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      backgroundColor: const Color.fromARGB(255, 22, 22, 22),
+      title: Column(
+        children: [
+          const Text(
+            'Seja Premium!',
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 18,
+            ),
+          ),
+          Container(
+            height: 15,
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.white10,
+                  width: 1,
+                  style: BorderStyle.solid,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 100,
+        child: Wrap(
+          children: [
+            Column(
+              children: const [
+                Icon(
+                  Icons.workspace_premium,
+                  size: 38,
+                  color: Color.fromARGB(255, 213, 32, 89),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    'Torne-se premium para desbloquear todos os recursos do nosso App.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            MaterialButton(
+              hoverColor: Colors.amber,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Ok',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ],
